06d12220c630a19f17083f5a547916a759ee2600
もっちゃん <s7056i3056@gmail.com>
2025-09-21 23:56:41 +0900

    refactor: queryとmutationをラップ

--- graphql/urqlClient.ts
+++ graphql/urqlClient.ts
@@ -1,4 +1,12 @@
-import { createClient, cacheExchange, fetchExchange } from 'urql';
+import {
+  createClient,
+  cacheExchange,
+  fetchExchange,
+  useQuery,
+  useMutation,
+  UseQueryArgs,
+} from 'urql';
+import { DocumentNode } from 'graphql';
 
 export const GRAPHQL_ENDPOINT = 'http://localhost:3000/graphql';
 
@@ -7,3 +15,57 @@ export const urqlClient = createClient({
   exchanges: [cacheExchange, fetchExchange],
   preferGetMethod: false,
 });
+
+// Query用の型定義
+type QueryOptions<T = any> = Omit<UseQueryArgs<any>, 'query'> & {
+  query: DocumentNode;
+};
+
+// Mutation用の型定義
+type MutationOptions<T = any> = {
+  query: DocumentNode;
+  variables?: any;
+  context?: any;
+};
+
+/**
+ * GraphQLクライアント層
+ *
+ * 目的:
+ * 1. urqlの直接的な依存を隠蔽
+ * 2. 統一されたAPIでクエリとミューテーションを実行
+ * 3. 将来的にクライアント実装を変更する際の影響範囲を限定
+ * 4. ビジネスロジックとGraphQL実装の分離
+ */
+export interface GraphQLClient {
+  query: <T = any>(options: QueryOptions<T>) => ReturnType<typeof useQuery<T>>;
+  mutation: <T = any>(
+    options: MutationOptions<T>,
+  ) => ReturnType<typeof useMutation<T>>;
+}
+
+/**
+ * GraphQLクライアントフック
+ *
+ * urqlClientで設定されたurqlクライアントを使用して、
+ * 統一されたAPIでGraphQL操作を実行する
+ */
+export const useGraphQLClient = (): GraphQLClient => {
+  return {
+    query: <T = any>(options: QueryOptions<T>) => {
+      return useQuery<T>(options as any);
+    },
+    mutation: <T = any>(options: MutationOptions<T>) => {
+      return useMutation<T>(options.query);
+    },
+  };
+};
+
+// 個別のフックも提供（より直接的な使用を希望する場合）
+export const useGraphQLQuery = <T = any>(options: QueryOptions<T>) => {
+  return useQuery<T>(options as any);
+};
+
+export const useGraphQLMutation = <T = any>(options: MutationOptions<T>) => {
+  return useMutation<T>(options.query);
+};
